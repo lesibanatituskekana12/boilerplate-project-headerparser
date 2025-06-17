6ac43e02554fcc502eecb0f88b9c8473b4f0af8a
Lesibana Titus Kekana <titusmahlatsekekana@gmail.com>
2025-06-17 16:01:07 +0000

    Completed header parser microservice

--- index.js
+++ index.js
@@ -1,30 +1,22 @@
-// index.js
-// where your node app starts
+const express = require('express');
+const app = express();
 
-// init project
-require('dotenv').config();
-var express = require('express');
-var app = express();
+app.set('trust proxy', true); // Needed in Gitpod to get correct IP
 
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-var cors = require('cors');
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
-
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
+app.get('/api/whoami', (req, res) => {
+  res.json({
+    ipaddress: req.ip,
+    language: req.headers['accept-language'],
+    software: req.headers['user-agent']
+  });
 });
 
-// listen for requests :)
-var listener = app.listen(process.env.PORT || 3000, function () {
+const listener = app.listen(process.env.PORT || 3000, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
